6b0bb3bddd68474ea127b19ae83b0eb975c5cf01
watanaberaiki <k021g1324@g.neec.ac.jp>
2023-05-10 11:52:24 +0900

    とちゅう

--- FbxModel.h
+++ FbxModel.h
@@ -2,6 +2,12 @@
 #include <DirectXMath.h>
 #include<vector>
 #include<DirectXTex.h>
+#include<Windows.h>
+#include<wrl.h>
+#include<d3d12.h>
+#include<d3dx12.h>
+
+
 
 struct Node 
 {
@@ -34,6 +40,22 @@ public:		//サブクラス
 	};
 public:
 	friend class FbxLoader;
+
+private:	//エイリアス
+	//Microsoft::WRL::を省略
+	template <class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
+	//DirectXを省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMMATRIX = DirectX::XMMATRIX;
+	using TexMetadata = DirectX::TexMetadata;
+	using ScratchImage = DirectX::ScratchImage;
+	//std::を省略
+	using string = std::string;
+	template<class T>using vector = std::vector<T>;
+
+
 private:
 	//モデル名
 	std::string name;
